611a97cb6098aa017e8930016fcd510b6806454c
ilseon_min <anaisun@gmail.com>
2023-06-23 00:20:37 +0900

    new_05

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import {HashRouter} from 'react-router-dom'
+import {BrowserRouter} from 'react-router-dom'
 import { Provider } from 'react-redux';
 import store from './pages/store'
 
@@ -11,9 +11,9 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <HashRouter basename='process.env.PUBLIC_URL'>
+      <BrowserRouter basename='process.env.PUBLIC_URL'>
         <App />
-      </HashRouter>
+      </BrowserRouter>
     </Provider>
   </React.StrictMode>
 );
